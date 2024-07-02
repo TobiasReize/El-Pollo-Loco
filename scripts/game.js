bf0f1be40a37bc0c9d166b0b9bd72ca7cd32eda5
Tobias Reize <reizeto@web.de>
2024-07-02 09:30:51 +0200

    added first classes

--- scripts/game.js
+++ scripts/game.js
@@ -1,13 +1,10 @@
 let canvas;
-let ctx;
-let character = new Image();
+let world;
 
 
 function init() {
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-
-    character.src = '../assets/img/2_character_pepe/2_walk/W-21.png';
-
-    ctx.drawImage(character, 20, 20, 50, 150);  //wird erst mal nicht ausgeführt, da das Bild des Characters zuerst vollständig geladen sein muss! (mit setTimeout würde es funktionieren!)
+    world = new World(canvas);
+    
+    console.log('My character is', world.character);
 }
