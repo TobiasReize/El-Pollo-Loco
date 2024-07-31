3e6f6cd1f5c97811e3a5b391bdf3d697e897a16c
Tobias Reize <reizeto@web.de>
2024-07-31 15:10:20 +0200

    added sound-function for start-screen and updated fullscreen-function

--- scripts/game.js
+++ scripts/game.js
@@ -3,46 +3,10 @@ let world;
 let keyboard = new Keyboard();
 let intervalIDs = [];   //Sammler für alle Intervalle
 let introSound = new Audio('assets/audio/intro.mp3');
+let introSoundIntervalID = 0;
 let gameSound = new Audio('assets/audio/game-music.mp3');
 let winScreenSound = new Audio('assets/audio/you-win.mp3');
 let gameOverSound = new Audio('assets/audio/game-over-music.mp3');
-
-
-function init() {
-    introSound.autoplay = true;
-    introSound.volume = 0.4
-    introSound.loop = true;
-
-    let promise = introSound.play();
-    console.log(promise);
-    setInterval(() => {
-        if (introSound.currentTime > 41) {
-            introSound.currentTime = 0;
-        }
-    }, 200);
-}
-
-
-function startGame() {
-    introSound.pause();
-    gameSound.volume = 0.1;
-    gameSound.loop = true;
-    gameSound.play();
-    document.getElementById('overlay_start_screen').classList.add('d-none');
-    initLevel();
-    canvas = document.getElementById('canvas');
-    world = new World(canvas, keyboard);    //Der Instanz der "World" wird das "canvas" und die Instanz des "Keyboard" übergeben!
-}
-
-
-function showInstructions() {
-    document.getElementById('overlay_instructions').classList.remove('d-none');
-}
-
-
-function closeInstructions() {
-    document.getElementById('overlay_instructions').classList.add('d-none');
-}
 
 
 window.addEventListener('keydown', (e) => {
@@ -99,9 +63,119 @@ window.addEventListener('keyup', (e) => {
 });
 
 
-function muteAllSounds() {
-    let audioCollection = document.getElementsByTagName('audio');
-    console.log(audioCollection);
+// Start screen:
+function startGame() {
+    introSound.pause();
+    gameSound.volume = 0.1;
+    gameSound.loop = true;
+    gameSound.play();
+    document.getElementById('overlay_start_screen').classList.add('d-none');
+    initLevel();
+    canvas = document.getElementById('canvas');
+    world = new World(canvas, keyboard);    //Der Instanz der "World" wird das "canvas" und die Instanz des "Keyboard" übergeben!
+}
+
+
+function showInstructions() {
+    document.getElementById('overlay_instructions').classList.remove('d-none');
+}
+
+
+function closeInstructions() {
+    document.getElementById('overlay_instructions').classList.add('d-none');
+}
+
+
+function activateIntroSound() {
+    document.getElementById('start_screen_sound_off').classList.add('d-none');
+    document.getElementById('start_screen_sound_on').classList.remove('d-none');
+    introSound.volume = 0.2;
+    introSound.loop = true;
+    introSound.play();
+    introSoundIntervalID = setInterval(() => {
+        if (introSound.currentTime > 41) {
+            introSound.currentTime = 0;
+        }
+    }, 200);
+}
+
+
+function deactivateIntroSound() {
+    document.getElementById('start_screen_sound_off').classList.remove('d-none');
+    document.getElementById('start_screen_sound_on').classList.add('d-none');
+    introSound.pause();
+    introSound.currentTime = 0;
+    clearInterval(introSoundIntervalID);
+}
+
+
+// Game screen:
+function showFullscreen() {
+    let canvas = document.getElementById('canvas');
+    let gameInfoContainer = document.getElementById('game_info_container');
+
+    checkFullscreen();
+    canvas.classList.add('canvas-fullscreen');
+    gameInfoContainer.classList.add('game-info-container-fullscreen');
+
+    document.getElementById('game_imprint_container').classList.add('d-none');
+    document.getElementById('headline').classList.add('d-none');
+
+    document.getElementById('icon_fullscreen').classList.add('d-none');
+    document.getElementById('icon_exit_fullscreen').classList.remove('d-none');
+}
+
+
+function checkFullscreen() {
+    let fullscreen = document.getElementById('fullscreen');
+    if (fullscreen.requestFullscreen) {
+	    fullscreen.requestFullscreen();
+	  } else if (fullscreen.webkitRequestFullscreen) { /* Safari */
+	    fullscreen.webkitRequestFullscreen();
+	  } else if (fullscreen.msRequestFullscreen) { /* IE11 */
+	    fullscreen.msRequestFullscreen();
+	  }
+}
+
+
+function removeFullscreen() {
+    let canvas = document.getElementById('canvas');
+    let gameInfoContainer = document.getElementById('game_info_container');
+
+    checkExitFullscreen();
+    canvas.classList.remove('canvas-fullscreen');
+    gameInfoContainer.classList.remove('game-info-container-fullscreen');
+
+    document.getElementById('game_imprint_container').classList.remove('d-none');
+    document.getElementById('headline').classList.remove('d-none');
+
+    document.getElementById('icon_fullscreen').classList.remove('d-none');
+    document.getElementById('icon_exit_fullscreen').classList.add('d-none');
+}
+
+
+function checkExitFullscreen() {
+    if (document.exitFullscreen) {
+	    document.exitFullscreen();
+	  } else if (document.webkitExitFullscreen) { /* Safari */
+	    document.webkitExitFullscreen();
+	  } else if (document.msExitFullscreen) { /* IE11 */
+	    document.msExitFullscreen();
+	  }
+}
+
+
+function deactivateGameSounds() {
+    document.getElementById('game_sound_on').classList.add('d-none');
+    document.getElementById('game_sound_off').classList.remove('d-none');
+    let test = document.querySelectorAll("audio");
+    console.log(test);
+}
+
+
+function activateGameSounds() {
+    document.getElementById('game_sound_on').classList.remove('d-none');
+    document.getElementById('game_sound_off').classList.add('d-none');
 }
 
 
@@ -163,37 +237,4 @@ function stopAllIntervals() {                           //Funkion, die alle Inte
     for (let i = 0; i < 999; i++) {
         window.clearInterval(i);
     }
-}
-
-
-function showFullscreen() {
-    let fullscreen = document.getElementById('fullscreen');
-    let canvas = document.getElementById('canvas');
-    let gameInfoContainer = document.getElementById('game_info_container');
-
-    fullscreen.requestFullscreen();
-    canvas.classList.add('canvas-fullscreen');
-    gameInfoContainer.classList.add('game-info-container-fullscreen');
-
-    document.getElementById('game_imprint_container').classList.add('d-none');
-    document.getElementById('headline').classList.add('d-none');
-
-    document.getElementById('icon_fullscreen').classList.add('d-none');
-    document.getElementById('icon_exit_fullscreen').classList.remove('d-none');
-}
-
-
-function removeFullscreen() {
-    let canvas = document.getElementById('canvas');
-    let gameInfoContainer = document.getElementById('game_info_container');
-
-    document.exitFullscreen();
-    canvas.classList.remove('canvas-fullscreen');
-    gameInfoContainer.classList.remove('game-info-container-fullscreen');
-
-    document.getElementById('game_imprint_container').classList.remove('d-none');
-    document.getElementById('headline').classList.remove('d-none');
-
-    document.getElementById('icon_fullscreen').classList.remove('d-none');
-    document.getElementById('icon_exit_fullscreen').classList.add('d-none');
 }
